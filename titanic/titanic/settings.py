7c775d89c2807c24779b3adcaf0df8ab27494d67
Irvin Campos <irvincampos76@gmail.com>
2022-03-04 15:16:10 -0800

    secret

--- titanic/titanic/settings.py
+++ titanic/titanic/settings.py
@@ -15,12 +15,13 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&8e=)%=s*1b%fc888i*oe0p6tcoq9r^(g(@!!%2$c1kx26!s2z'
+from . import secret
+import os
+SECRET_KEY = secret.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
